fb1564a8fc12b316fe88ec1b15040274f110c56a
FIL708 <filip.zebrowski4013@gmail.com>
2022-08-16 20:03:01 +0200

    create a comp

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <header>To jest header</header>
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+export default function Main() {
+    return (
+        <main>To jest main</main>
+    )
+}
