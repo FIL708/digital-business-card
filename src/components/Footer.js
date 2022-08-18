4e661d45d1fdf2d79559a632b6557f5e28eee850
FIL708 <filip.zebrowski4013@gmail.com>
2022-08-18 20:55:02 +0200

    add icons and link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,31 @@
-import { FaLinkedin } from "react-icons/fa";
+import { FaLinkedin, FaFacebookSquare, FaInstagramSquare, FaGithubSquare  } from "react-icons/fa";
 
 export default function Main() {
     return (
         <footer>
             <a 
-                href="www.linkedin.com/in/filipzebrowski"
-                rel="noopener">
-                <FaLinkedin/>
+            href="https://www.linkedin.com/in/filipzebrowski/" 
+            target="_blank"
+            rel="noopener noreferrer">
+            <FaLinkedin/>
+            </a>
+            <a 
+            href="https://www.facebook.com/f.zebrowski/" 
+            target="_blank"
+            rel="noopener noreferrer">
+            <FaFacebookSquare/>
+            </a>
+            <a 
+            href="https://www.instagram.com/fil_ski9/" 
+            target="_blank"
+            rel="noopener noreferrer">
+            <FaInstagramSquare/>
+            </a>
+            <a 
+            href="https://github.com/FIL708" 
+            target="_blank"
+            rel="noopener noreferrer">
+            <FaGithubSquare/>
             </a>
         </footer>
     )
