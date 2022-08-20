4259437e68d9177f3007db3f8d8d20822af3cdf1
FIL708 <filip.zebrowski4013@gmail.com>
2022-08-20 21:07:07 +0200

    end of styling main

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,22 +7,24 @@ export default function Main() {
             <section className="main--introduction">
                 <h3 className="main--name">Filip Żebrowski</h3>
                 <h4 className="main--title">Frontend Developer</h4>
-                <a 
-                    href="mailto: example@exaple.com"
-                    className='main--mail button'>
-                    <GrMail className="button-icons"/>Email
-                </a>
-                <a 
-                    href="https://www.linkedin.com/in/filipzebrowski/" 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className='main--linkedin button'>
-                    <FaLinkedin className="button-icons"/> LinkedIn
-                </a>
+                <div className="buttons">
+                    <a 
+                        href="mailto: example@exaple.com"
+                        className='main--mail button'>
+                        <GrMail className="button-icons"/>Email
+                    </a>
+                    <a 
+                        href="https://www.linkedin.com/in/filipzebrowski/" 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='main--linkedin button'>
+                        <FaLinkedin className="button-icons"/> LinkedIn
+                    </a>
+                </div>
             </section>
             <section className="main--description">
                 <h4>About</h4>
-                <p>Something</p>
+                <p className="main--interestsPara">Veterinary diagnostician with 3+ years of experience. I have been interested in IT industry for several years. My goal is to develop my career in this branch. </p>
                 <h4>Interests</h4>
                 <p className="main--interestsPara">Programming. Web development. Database. PC building. Veterinary diagnostics. New technology. Mountain hike.</p>
             </section>
